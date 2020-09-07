507029b31c7390a11995a6063081a4d80978e38d
mperezi <mperezibars@gmail.com>
2020-09-07 07:36:34 +0200

    [sdkman] Lazy loading for Maven

--- sdkman/init.zsh
+++ sdkman/init.zsh
@@ -2,14 +2,22 @@ SDKMAN_DIR="/usr/local/sdkman"
 
 if [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]]; then
 
-# https://frederic-hemberger.de/articles/speed-up-initial-zsh-startup-with-lazy-loading/
-sdk() {
-	unfunction "$0"
-
+init_sdkman() {
 	if (( ! ${+SDKMAN_VERSION} )); then
 		source "$SDKMAN_DIR/bin/sdkman-init.sh"
 	fi
+}
 
+# https://frederic-hemberger.de/articles/speed-up-initial-zsh-startup-with-lazy-loading/
+sdk() {
+	unfunction "$0"
+	init_sdkman
+	$0 "$@"
+}
+
+mvn() {
+	unfunction "$0"
+	init_sdkman
 	$0 "$@"
 }
 
