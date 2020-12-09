21e475a68a54a161943bfe0e0de09f67e5cdb9a4
mperezi <mperezibars@gmail.com>
2020-12-09 13:24:10 +0100

    [sdkman] sdk env chpwd hook

--- sdkman/init.zsh
+++ sdkman/init.zsh
@@ -1,24 +1,55 @@
 SDKMAN_DIR="/usr/local/sdkman"
 
-if [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]]; then
+# https://mharrison.org/post/bashfunctionoverride/
+copy_function() {
+	local ORIG_FUNC=$(declare -f $1)
+	local NEWNAME_FUNC="$2${ORIG_FUNC#$1}"
+	eval "$NEWNAME_FUNC"
+}
+
+if [[ -d ${SDKMAN_DIR} ]]; then
+
+# https://unix.stackexchange.com/a/204944
+for candidate in ${SDKMAN_DIR}/candidates/*(N); do
+	if [[ -d "${candidate}/current/bin" ]]; then
+		PATH="${candidate}/current/bin:${PATH}"
+	fi
+done
 
 init_sdkman() {
 	if (( ! ${+SDKMAN_VERSION} )); then
-		source "$SDKMAN_DIR/bin/sdkman-init.sh"
+		source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+		copy_function sdk sdk_command
+		copy_function sdk_proxy sdk
 	fi
 }
 
-# https://frederic-hemberger.de/articles/speed-up-initial-zsh-startup-with-lazy-loading/
-sdk() {
-	unfunction "$0"
-	init_sdkman
-	$0 "$@"
+sdk_fuzzy_find_local_version() {
+	local candidate=$1
+	__sdkman_build_version_csv $candidate | tr ',' '\n' | sort -r -n | fzf -d '\.' --nth 1
 }
 
-mvn() {
-	unfunction "$0"
+# https://frederic-hemberger.de/articles/speed-up-initial-zsh-startup-with-lazy-loading/
+sdk_proxy() {
 	init_sdkman
-	$0 "$@"
+	if (( $# == 2 )) && [[ $1 =~ ^(u|use)$ ]]; then
+		local candidate=$2
+		sdk_command use $candidate $(sdk_fuzzy_find_local_version $candidate)
+	else
+		sdk_command "$@"
+	fi
 }
+copy_function sdk_proxy sdk
+
+sdkman_auto_env() {
+	if [[ -f .sdkmanrc ]]; then
+		sdk env
+	elif [[ -n $SDKMAN_ENV ]] && [[ ! $PWD =~ ^$SDKMAN_ENV ]]; then
+		sdk env clear
+	fi
+}
+add-zsh-hook chpwd sdkman_auto_env
+
+sdkman_auto_env
 
 fi
